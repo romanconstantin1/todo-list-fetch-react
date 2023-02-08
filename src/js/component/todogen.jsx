a915f5cffca52a79c27f035c3ef90cd6704c6bd7
romanconstantin1 <romanconstantin1@gmail.com>
2023-02-08 19:45:36 +0000

    feat: can now add list entries

--- src/js/component/todogen.jsx
+++ src/js/component/todogen.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const listItemGen = () => { 
-	return (
-		<li className="list-group-item d-flex w-100 justify-content-between">
-			list item 1
-			<span type="button">X</span>
-		</li>
-	)}
-
+let itemList = []
 
 const ToDoGen = () => {
+
+	const [text, inputText] = useState("")
+	const [key, newKey] = useState(0)
+
+
+	const inputVal = (event) => {
+		if (event.key === "Enter") {
+			let newEntry = <li key={key} className="list-group-item d-flex w-100 justify-content-between">
+				{event.target.value}
+				<span type="button">X</span>
+				</li>	
+			if (event.target.value != "") {
+				console.log(key)
+				itemList.push(newEntry)
+				inputText(event.target.value)
+				newKey(key+1)
+		}
+		}
+	}
+
+	const listGen = () => {
+		let newItem = []
+		if (itemList.length == 0) return null
+		else {
+			for (let i=0; i<= itemList.length-1; i++) {newItem.push(itemList[i])}
+		}
+		return newItem
+	}
+
 	return (
 		<div className="row justify-content-center">
 			<div className="col-4">
 			<h1 className="mt-5 text-center">todo list</h1>
 			<ul className="shadow list-group mt-3">
-				{listItemGen()}
-				{listItemGen()}
-				{listItemGen()}
-				{listItemGen()}
-				{listItemGen()}
-				{listItemGen()}
+				<input 
+					type="text" 
+					className="list-group-item input-group" 
+					placeholder="What do you need to do today?"
+					onKeyUp={(event) => {inputVal(event)}}
+				/>
+				{listGen()}
 			</ul>
 			</div>
 		</div>

3720c46212f2b50d1c3006d1a0f89634c82db5b9
romanconstantin1 <romanconstantin1@gmail.com>
2023-02-09 16:30:14 +0000

    feat: added entry removal

--- src/js/component/todogen.jsx
+++ src/js/component/todogen.jsx
@@ -7,25 +7,31 @@ const ToDoGen = () => {
 
 	const [listOfEntries, setItemList] = useState([])
 
+	const removeEntry = (entryId) => {
+		let newList = listOfEntries.map(a => ({...a}))
+		newList.splice((newList.findIndex(x => x.id === entryId)), 1)
+		setItemList(newList)
+	} 
+
 	return (
 		<div className="row justify-content-center">
 			<div className="col-4">
-			<h1 className="mt-5 text-center">todo list</h1>
+			<h1 className="mt-5 text-center">minimalist todo list</h1>
 			<ul className="shadow list-group mt-3">
 				<input 
 					type="text" 
 					className="list-group-item input-group" 
-					placeholder="What else do you need to do today?"
+					placeholder="What do you need to do today?"
 					onKeyUp={(event) => {
 						if (event.key === "Enter" && event.target.value !== "") {
-							setItemList([...listOfEntries, { id: key++, entry: event.target.value }])
+							setItemList([...listOfEntries, { id: key++, listItem: event.target.value }])
 						}
 					}}
 				/>
 				{listOfEntries.map(entry => 
 					<li key={entry.id} className="list-group-item d-flex w-100 justify-content-between">
-					{entry.entry}
-					<span type="button" onClick={() => {/*do stuff here*/}}>X</span>
+					{entry.listItem}
+					<span type="button" onClick={() => {removeEntry(entry.id)}}>X</span>
 					</li>)}
 			</ul>
 			</div>

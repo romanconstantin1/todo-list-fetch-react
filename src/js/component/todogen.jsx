e6929d66f9ac62dd3287c53da897a9bea06f2c70
romanconstantin1 <romanconstantin1@gmail.com>
2023-02-10 20:36:37 +0000

    feat: delete button shows on hover only

--- src/js/component/todogen.jsx
+++ src/js/component/todogen.jsx
@@ -5,13 +5,14 @@ let key = 0
 
 const ToDoGen = () => {
 
-	const [itemList, setItemList] = useState([])
-	const [value, setValue] = useState('')
-	const [vis, setVis] = useState(false)
+	const [itemList, setItemList] = useState([]) //holds values for list entries
+	const [value, setValue] = useState('') //clears text field on submit
+	const [vis, setVis] = useState({id: null}) //visibility check for delete button
 
-	const removeEntry = (entryId) => {
-		let newList = itemList.map(a => ({...a}))
-		newList.splice((newList.findIndex(x => x.id === entryId)), 1)
+
+	const removeItem = (entryId) => {
+		let newList = itemList.map(a => ({...a})) //make shallow copy of the list of entried
+		newList.splice((newList.findIndex(x => x.id === entryId)), 1) //remove entry according to id
 		setItemList(newList)
 	}
 	
@@ -21,17 +22,16 @@ const ToDoGen = () => {
 
 	const taskCounter = () => {
 		let mult = "s"
-		let numberOfTasks = "No"
-		let congrats = "- hooray!"
+		let tasksLeft = "no"
+		let yay = "- hooray!"
 
-		if (itemList.length == 1) {numberOfTasks = 1; mult = ""; congrats = ""}
-		if (itemList.length > 1) {numberOfTasks = itemList.length; mult = "s"; congrats = ""}
+		if (itemList.length == 1) {tasksLeft = 1; mult = ""; yay = ""}
+		if (itemList.length > 1) {tasksLeft = itemList.length; mult = "s"; yay = ""}
 
 		return <li className="list-group-item d-flex w-100 align-middle justify-content-between">
-			<small>{numberOfTasks} task{mult} left {congrats}</small>
+			<small>{tasksLeft} task{mult} left {yay}</small>
 		</li>
 	}
-
 
 	return (
 		<div className="row justify-content-center">
@@ -43,22 +43,23 @@ const ToDoGen = () => {
 					value={value} 
 					className="list-group-item input-group"
 					placeholder="What do I need to do today?"
-					onChange={(event) => valueChange(event)}
+					onChange={(event) => valueChange(event)} //continuously passes entry to a separate placeholder state
 					onKeyUp={(event) => {
-						if (event.key === "Enter" && event.target.value !== "") {
-							setItemList([...itemList, { id: key++, listItem: event.target.value }]);
-							setValue('')
+						if (event.key === "Enter" && event.target.value !== "") { // checks that the user has actually entered something
+							setItemList([...itemList, { id: key++, listItem: event.target.value }]); //id is used as a key in the list item as well
+							setValue('') //reset entry field to blank
 						}
 					}}
 				/>
 				{itemList.map(entry => 
 					<li key={entry.id} 
 					className="list-group-item d-flex w-100 align-middle justify-content-between"
-					onMouseEnter={() => setVis(true)}
-					onMouseLeave={() => setVis(false)}>
+					onMouseOver={() => setVis({id:(entry.id)})} //passes the entry id to the visibility check state
+					onMouseLeave={() => setVis({id:null})} //reset visibility check state
+					> 
 					<b>{entry.listItem}</b>
-					{vis && ( //add logic here for individually visible delete buttons
-					<span type="button" style={{color: "red"}} onClick={() => {removeEntry(entry.id)}}>delete</span>)}
+					{entry.id === vis.id && ( //button will only exist when the visibility state & list entry id match
+					<span type="button" style={{color: "red"}} onClick={() => {removeItem(entry.id)}}>delete</span>)}
 					</li>)}
 				{taskCounter()}
 			</ul>
